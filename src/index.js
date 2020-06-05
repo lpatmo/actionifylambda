10d71d5169980ceb273513efbf2702515b5e09f5
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-06-04 19:10:07 -0700

    auth0 so far

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "./utils/react-auth0-spa";
+import history from "./utils/history";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.DOMAIN}
+    clientId={process.env.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
