07efcba452ef3ff540d563c46224b15b46b52c31
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-06-06 01:55:04 -0700

    Check in auth0 files

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="page dashboard">
+      <div>
+        <h2>Welcome Persom</h2>
+      </div>
+    </div>
+  );
+}
